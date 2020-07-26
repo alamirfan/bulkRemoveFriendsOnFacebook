4bef5c803cf9ebe2b6fd24daaa5321d1ad3b8562
irfan-alam <irfan.alam@fyle.in>
2020-07-26 18:55:08 +0530

    code full refreshed, async await with timeout added

--- bulkRemoveFriendsOnFacebook.js
+++ bulkRemoveFriendsOnFacebook.js
@@ -1,47 +1,40 @@
-var removeFriends = (function() {
-  var fb_dtsg = "CHANGE_THIS"; //Required
-  var notToRemoveIds = ["ID1", "ID2", "ID3", "..."] //Optional
-  var removeFriends = {};
+var wait = (delay, ...args) => new Promise(resolve => setTimeout(resolve, delay, ...args));
 
-  var getFriendIds = function() {
-    var ids = [];
-    var frList = document.getElementsByClassName('_698');
-    for (i = 0; i < frList.length; i++) {
-      var id = frList[i].querySelectorAll('div > div > div > div > div > button')[1].getAttribute('data-profileid');
-      ids.push(id);
-    }
-    return ids;
+var getFriendIds = function() {
+  var ids = [];
+  var frList = document.getElementsByClassName('_698');
+  for (i = 0; i < frList.length; i++) {
+    var id = frList[i].querySelectorAll('div > div > div > div > div > button')[1].getAttribute('data-profileid');
+    ids.push(id);
   }
+  return ids;
+}
 
-  var makeRemoveCall = function(uid, fb_dtsg) {
-    var http = new XMLHttpRequest();
-    var url = 'https://www.facebook.com/ajax/profile/removefriendconfirm.php';
-    var params = "uid=" + uid + "&fb_dtsg=" + fb_dtsg;
-    http.open('POST', url, true);
-    http.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+var makeRemoveCall = async (uid, fb_dtsg) => {
+  await wait(1000);
+  var http = new XMLHttpRequest();
+  var url = 'https://www.facebook.com/ajax/profile/removefriendconfirm.php';
+  var params = "uid=" + uid + "&fb_dtsg=" + fb_dtsg;
+  http.open('POST', url, false);
+  http.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
 
-    http.onreadystatechange = function() {
-      if (http.readyState == 4 && http.status == 200) {
-        console.log("Removed friend with ID: " + uid);
-      }
+  http.onreadystatechange = function() {
+    if (http.readyState == 4 && http.status == 200) {
+      console.log("Removed friend with ID: " + uid);
     }
-    http.send(params);
-    return true;
   }
+  http.send(params);
+};
 
-  removeFriends.start = function() {
-    console.log("Friend removal started");
-    var ids = getFriendIds();
-    for (i = 0; i < ids.length; i++) {
-      if (!notToRemoveIds.includes(ids[i])) {
-        makeRemoveCall(ids[i], fb_dtsg);
-      }
+async function removeFriends() {
+  console.log("Friend removal started");
+  var ids = getFriendIds();
+  for (i = 0; i < ids.length; i++) {
+    if (!notToRemoveIds.includes(ids[i])) {
+        await makeRemoveCall(ids[i], fb_dtsg);
     }
-    console.log("Friend removal completed");
   }
+  console.log("Friend removal completed");
+}
 
-  return removeFriends;
-})();
-
-// To start the removal
-removeFriends.start();
+removeFriends();
